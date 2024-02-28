cf0219af6efea7d35a0fcc44126af4adbaa2ba1f
Caribla <caribla4@gmail.com>
2024-02-28 22:03:34 +0100

    Feat - Get appointment by userId controller

--- src/controllers/appointmentsController.ts
+++ src/controllers/appointmentsController.ts
@@ -39,6 +39,34 @@ export const GetAppointmentById = async (req : Request, res : Response) => {
     }
 }
 
-export const GetAppointments = (req : Request, res : Response) => {
+export const GetAppointments = async (req : Request, res : Response) => {
+    try {
+        const userId = parseInt(req.query.userId as string, 10)
 
+        const appointments = await Appointment.find({
+            where: {
+                user: {id: userId}
+            }
+        })
+
+        if(!appointments){
+            return res.status(404).json({
+                success: false,
+                message: "Appointments from user not found on DB"
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Appointments from user retrieved from DB successfully",
+            data: appointments
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Get user appointments from DB failure",
+            error: error
+        });
+    }
 }

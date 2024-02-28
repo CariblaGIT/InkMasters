aab623a6794a05e72650c0c244f94dcddabf4402
Caribla <caribla4@gmail.com>
2024-02-28 13:58:52 +0100

    Fix - Import not removed from latest commit

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import { User } from "../models/User";
-import { IsNull } from "typeorm";
 
 export const GetUsers = async (req : Request, res : Response) => {
     try {

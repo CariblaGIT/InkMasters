ea0597c3b1934c1fa7cde4543b43ff9317fe3049
Caribla <caribla4@gmail.com>
2024-02-28 16:15:08 +0100

    Feat - Get services controller

--- src/controllers/servicesController.ts
+++ src/controllers/servicesController.ts
@@ -1,7 +1,29 @@
 import { Request, Response } from "express";
+import { Service } from "../models/Service";
 
-export const GetServices = (req : Request, res : Response) => {
-    
+export const GetServices = async (req : Request, res : Response) => {
+    try {
+        const allServices = await Service.find({
+            select : {
+                serviceName : true,
+                description : true,
+                id : false
+            }
+        })
+
+        return res.status(200).json({
+            success: true,
+            message: "Services retrieved from DB successfully",
+            data: allServices
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Get services from DB failure",
+            error: error
+        });
+    }
 }
 
 export const PostService = (req : Request, res : Response) => {

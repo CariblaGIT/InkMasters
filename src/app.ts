4064641422d4475ca46d67b15d789240603e884d
Caribla <caribla4@gmail.com>
2024-03-26 11:47:48 +0100

    Fix - Check if is neccessary the folder for deploy

--- src/app.ts
+++ src/app.ts
@@ -25,11 +25,11 @@ export const app : Application = express();
 // });
 // // Create a multer instance with the storage strategy
 // const upload = multer({ storage: storage });
-const upload = multer({ dest: __dirname + "/public/data/uploads" });
+const upload = multer({ dest: __dirname + "/public/uploads" });
 
 app.use(express.json());
 app.use(cors());
-app.use('/public', express.static(path.join(__dirname, "./public/data/uploads")));
+app.use('/public', express.static(path.join(__dirname, "./public/uploads")));
 
 /* 
 ========================================

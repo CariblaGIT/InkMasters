758067f7a27423657309545115d238bb9548e932
Caribla <caribla4@gmail.com>
2024-03-26 10:35:51 +0100

    Rebase - Returning to prev commit when change from diskStorage to path and changing express static

--- src/app.ts
+++ src/app.ts
@@ -25,13 +25,11 @@ export const app : Application = express();
 // });
 // // Create a multer instance with the storage strategy
 // const upload = multer({ storage: storage });
-const pathFiles = path.join(__dirname, "../public/uploads")
-
-const upload = multer({ dest: pathFiles });
+const upload = multer({ dest: './public/data/uploads/' });
 
 app.use(express.json());
 app.use(cors());
-app.use('/public', express.static(pathFiles));
+app.use('/public', express.static(path.join(__dirname, "./public/data/uploads/")));
 
 /* 
 ========================================
